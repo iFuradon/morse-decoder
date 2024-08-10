f2be4c39554116bbf723328ae49d172369ef5abb
iFuradon <iFuradon@gmail.com>
2024-08-11 00:13:35 +0400

    init: create code function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let decodedString = '';
+
+    for(let i = 0; i < expr.length; i += 10) {
+
+        let segm = expr.slice(i, i + 10);
+
+        if (segm === '**********'){
+            decodedString += ' ';
+        } else {
+            let morseSymbol = '';
+
+            for (let j = 0; j < segm.length; j += 2){
+                
+                let pairDigit = segm.slice(j, j + 2 );
+                
+                if (pairDigit === '10') {
+                    morseSymbol += '.';
+                } else if (pairDigit === '11') {
+                    morseSymbol += '-';
+                }
+            }
+
+            if (morseSymbol in MORSE_TABLE) {
+                decodedString += MORSE_TABLE[morseSymbol];
+            }
+        }
+    }
+
+    return decodedString;
 }
 
 module.exports = {
